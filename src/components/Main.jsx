99e316b39005bcb8dda454739dc570df49d01a24
seba-sierra <gerardosebastiansierra@gmail.com>
2023-09-28 11:06:14 -0300

    jueves 28/9

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import ClaseTres from './components/capitulos/ClaseTres';
 
 const Main = () => {
   return (
     <div>
         <h1>Main</h1>
+        <ClaseTres/>
         <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error odio vero enim eveniet perspiciatis neque architecto a pariatur recusandae molestiae harum accusantium, minima, cumque magnam asperiores veritatis provident? Quas, dolor.</p>
     </div>
   )
