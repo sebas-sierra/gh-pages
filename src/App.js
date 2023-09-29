536f1ce60b9a15591a1fffc45591c48fc1ae2516
seba-sierra <gerardosebastiansierra@gmail.com>
2023-09-29 11:07:37 -0300

    viernes 29/9 v2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
@@ -10,25 +9,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">
       <Header/>
       <Main/>
       <Footer/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>GitHub - pages</h1>
-        <p>Esto es un testing de GitHub-pages para armar un portfolio</p>
-        <p>Nuevo Commit lunes 25 / 9 / 2023</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div></BrowserRouter>
+    </BrowserRouter>
   );
 }
 
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 import ClaseTres from './capitulos/ClaseTres';
 import ClaseCuatro from './capitulos/ClaseCuatro';
 const Main = () => {
   return (
-    <div>
-        <h1>Main</h1>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error odio vero enim eveniet perspiciatis neque architecto a pariatur recusandae molestiae harum accusantium, minima, cumque magnam asperiores veritatis provident? Quas, dolor.</p>
-        <ClaseTres/>
-        <ClaseCuatro/>
-    </div>
+    <Container className='main'>
+        <Row>
+          <Col sm={8}>
+            <h1>Main</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Error odio vero enim eveniet perspiciatis neque architecto a pariatur recusandae molestiae harum accusantium, minima, cumque magnam asperiores veritatis provident? Quas, dolor.</p>
+            <ClaseTres/>
+            <ClaseCuatro/>
+          </Col>
+        </Row>
+    </Container>
   )
 }
 
