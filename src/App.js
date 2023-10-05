f4c9ba27f9e456e7cfeb6845a42097d98b6d7726
seba-sierra <gerardosebastiansierra@gmail.com>
2023-10-04 21:15:38 -0300

    miercoles 4/10 v5

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Header/>
       <Main/>
       <Footer/>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
