6763eff16ac5f4856c97fadb69c6d3b08025c2dc
seba-sierra <gerardosebastiansierra@gmail.com>
2023-10-26 11:12:41 -0300

    jueves 26/10 v1

--- src/components/capitulos/ItemListContainer.jsx
+++ src/components/capitulos/ItemListContainer.jsx
@@ -1,9 +1,35 @@
 import React from 'react';
 import ItemList from './ItemList'
+import { useEffect, } from 'react';
+import { products } from '../../mock/products';
 
 const ItemListContainer = () => {
+  //const [ Item, setItem ] = useState ([]);
+  
+  useEffect (()=>{
+    const getProducts = () => {
+      return new Promise((res, rej) => {
+        setTimeout(() => {
+          res(products);
+        }, 3500);
+      });
+    };
+
+  getProducts()
+  .then((res) => {
+    console.log('res', res)
+  })
+  .catch((error) => {
+    console.log('res', error);
+  });
+
+  }, []);
+
   return (
+    <>
+    <p>PRODUCTOS</p>
     <ItemList/>
+    </>
   )
 }
 
