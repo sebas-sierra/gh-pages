15ee92eb0348fbcfbb3c13a74e33fa6b943df685
seba-sierra <gerardosebastiansierra@gmail.com>
2024-08-05 14:54:33 -0300

    Lun 5/8

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,6 +1,6 @@
 import { React, useContext } from 'react';
 import { CartContext } from '../../context/CartContext';
-import { Button, Row, Col, Card } from 'react-bootstrap';
+import { Button, Image, Row, Col, Card, Table } from 'react-bootstrap';
 
 const Cart = () => {
     const { cart, deleteOne, deleteAll, totalPrecio } = useContext(CartContext);
@@ -8,43 +8,44 @@ const Cart = () => {
         <div>
             <h3>aca voy a mostrar lo s elementos enviados al cart por la funcion onAdd</h3>
             <p>En esta vista voy a poder visualizar los items enviados a mi cart desde el itemDetail llamandolos desde mi contexto CartContext</p>
-            
-
-            {cart.map((prod) => {
-                return (
-               
-                    <Col>
-                    <Card className='mb-3'>
-                        <Row className='g-0'>
-                        <Col className='col-md-4'>
-                            <Card.Img  className='img-fluid rounded-start' src={prod.img}alt={prod.title} width="180px" ></Card.Img>
-                        </Col>
-                        <Col className='col-md-8'>
-                            <Card.Body>
-                                <Card.Title>{prod.title}</Card.Title>
-                                <Card.Text>Cant. de unidades: {prod.cantidad}</Card.Text>
-                                <Button onClick={() => deleteOne(prod.id)}>Eliminar</Button>
-                                {/* <button onClick={() => deleteOne(prod.id)}>Eliminar</button> */}
-                            </Card.Body>
-                        </Col>
-                        </Row>
-                    </Card>
-                    {/* <img src={prod.img} alt={prod.title} width="180px" />
-                    <div>
-                        <h3>{prod.title}</h3>
-                        <h4>Cant. de unidades: {prod.cantidad}</h4>
-                        <button onClick={() => deleteOne(prod.id)}>Eliminar</button>
-                    </div> */}
-                    </Col>
-                 )
-            })}
-
-            
-            <h3>TOTAL: $.- <span>{totalPrecio()}</span></h3>
-            <button onClick={deleteAll}>Vaciar el carrito</button>
-            <Button href='/gh-pages/#/checkout'>Checkout</Button>
-            <button>checkout</button>
-            
+            <Table responsive >
+                <thead>
+                    <tr bordered>
+                        <th xs={6} md={4}> #</th>
+                        <th>Producto</th>
+                        <th>Cantidad</th>
+                        <th>Precio</th>
+                        <th>Eliminar</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {cart.map((prod) => {
+                        return (
+                            <tr>
+                                <td><Image src={prod.img} alt={prod.title} width={171} height={180} thumbnail /></td>
+                                <td><Card.Title>{prod.title}</Card.Title> <Card.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sed quo impedit accusantium aspernatur dolorem facilis repellat incidunt quasi ducimus iste vel, iusto doloribus quidem officia eum eius totam dolore nulla?</Card.Text></td>
+                                <td><Card.Text>{prod.cantidad}</Card.Text></td>
+                                <td>${prod.price}.-</td>
+                                <td><Button onClick={() => deleteOne(prod.id)}>Eliminar</Button></td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </Table>
+            <Card>
+                <Card.Header>Totales del carrito<Button onClick={deleteAll}>Vaciar el carrito</Button></Card.Header>
+                <Card.Body>
+                    subtotal: $. <span>{totalPrecio()}</span> -
+                    <hr />
+                    envio: $. 3000 -
+                    <hr />
+                    total: $. (sumar subtotal + envio) -
+                    
+                </Card.Body>
+                <Card.Footer>
+                    <Button href='/gh-pages/#/checkout'>Checkout</Button>
+                </Card.Footer>
+            </Card>
         </div>
     )
 }
